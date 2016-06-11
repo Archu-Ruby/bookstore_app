1f438d09b56a968ebf7acb328e5405ba986ffd59
Rails Project <archanapanjakadi@gmail.com>
2016-06-11 18:50:22 +0000

    Optimized code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,50 +2,45 @@ class BooksController < ApplicationController
   def new
     @page_title= 'Add Book'
     @book = Book.new
-     @category = Category.new
-      @publisher= Publisher.new
-       @author = Author.new
+    @category = Category.new
+    @publisher= Publisher.new
+    @author = Author.new
   end
-
   def create
-@book=Book.new(book_params)
-if @book.save
-flash[:notice]= "Book is sucessfully created"
-redirect_to  books_path
-else
-render 'new'
-end
+    @book=Book.new(book_params)
+    if @book.save
+       flash[:notice]= "Book is sucessfully created"
+       redirect_to  books_path
+    else
+      render 'new'
+    end
   end
   def update
-   @book=Book.find(params[:id])
- @book.update(book_params)
-flash[:notice]= "Book is sucessfully updated"
-redirect_to  books_path
-render 'edit'
-
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice]= "Book is sucessfully updated"
+    redirect_to  books_path
+    render 'edit'
   end
   def edit
     @book=Book.find(params[:id])
   end
-
   def destroy
- @book=Book.find(params[:id])
-@book.delete
-flash[:notice]= "Book is sucessfully deleted"
-redirect_to  books_path
-
+    @book=Book.find(params[:id])
+    @book.delete
+    flash[:notice]= "Book is sucessfully deleted"
+    redirect_to  books_path
   end
   def index
     @books=Book.all
-@categories=Category.all
+    @categories=Category.all
   end
-
   def show
-@book=Book.find(params[:id])
-@categories=Category.all
+    @book=Book.find(params[:id])
+    @categories=Category.all
   end
   private
   def book_params
     params.require(:book).permit(:title,:category_id,:author_id,:publisher_id,:isbn,:price,:but,:coverpath,:format,:excerpt,:pages)
-end
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,52 +3,40 @@ class CategoriesController < ApplicationController
     @page_title = 'Add New Category'
     @category = Category.new
   end
-
   def create
     @category=Category.new(category_params)
-if @category.save
-flash[:notice]= "Category is sucessfully created"
-redirect_to  categories_path
-else
-render 'new'
-end
+    if @category.save
+      flash[:notice]= "Category is sucessfully created"
+      redirect_to  categories_path
+    else
+    render 'new'
+    end
   end
-
   def update
     @category = Category.find(params[:id])
-
     @category.update(category_params)
-
     flash[:notice] = 'Category Updated'
-
     redirect_to categories_path
   end
-
   def edit
     @category = Category.find(params[:id])
   end
-
   def destroy
     @category = Category.find(params[:id])
-
     @category.destroy
-
     flash[:notice] = 'Category Removed'
-
     redirect_to categories_path
   end
   def index
     @categories=Category.all
   end
-
   def show
-
     @category=Category.find(params[:id])
-     @categories=Category.all
+    @categories=Category.all
     @books = @category.books
   end
   private
   def category_params
-  params.require(:category).permit(:name)
+    params.require(:category).permit(:name)
   end
 end
